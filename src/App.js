00b149a51c13251d173751a531224840e2591aca
Achhaya Pathak <93811742+achhayapathak@users.noreply.github.com>
2023-04-06 21:44:01 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ const App = () => {
 
     return(
         <div className="app">
-            <h1>Sasta Netflix</h1>
+            <h1>Netflix</h1>
             
             <div className="search">
                 <input 
